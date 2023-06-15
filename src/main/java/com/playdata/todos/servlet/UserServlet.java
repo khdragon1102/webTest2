dc1b7098a784862e5e7b82a8c381933cd0c86034
khdaragon1102 <khdragon1102@gmail.com>
2023-06-15 10:48:29 +0900

    user post

--- src/main/java/com/playdata/todos/servlet/UserServlet.java
+++ src/main/java/com/playdata/todos/servlet/UserServlet.java
@@ -9,12 +9,21 @@ import java.io.IOException;
 public class UserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setStatus(200);
         req.getRequestDispatcher("views/user.html").forward(req, resp);
         super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String name = req.getParameter("name");
+        System.out.println("username = " + username);
+        System.out.println("password = " + password);
+        System.out.println("name = " + name);
+        resp.setStatus(201);
+        resp.sendRedirect("/user");
+        //super.doPost(req, resp);
     }
 }
