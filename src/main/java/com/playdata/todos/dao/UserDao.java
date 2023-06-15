27173f2396603463b580f5ec56c93f88ae53c8a1
khdaragon1102 <khdragon1102@gmail.com>
2023-06-15 15:27:04 +0900

    login2

--- src/main/java/com/playdata/todos/dao/UserDao.java
+++ src/main/java/com/playdata/todos/dao/UserDao.java
@@ -5,7 +5,10 @@ import com.playdata.todos.dto.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserDao {
     public void insert(User user){
@@ -22,4 +25,49 @@ public class UserDao {
             throw new RuntimeException(e);
         }
     }
+
+    public boolean login(String id, String password){
+        List<User> users = new ArrayList<>();
+        Connection conn = new JdbcConnection().getJdbc();
+        String sql = "select *" +
+                "from users " +
+                "where username=? and password=?";
+
+        try {
+            PreparedStatement pst = conn.prepareStatement(sql);
+            pst.setString(1, id);
+            pst.setString(2, password);
+            ResultSet result = pst.executeQuery();
+
+            while(result.next()) {
+                users.add(makeUser(result));
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            conn.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return users.size() != 0;
+    }
+
+    private User makeUser(ResultSet resultset){
+        try {
+            Integer userid = resultset.getInt("id");
+            String username = resultset.getString("username");
+            String name = resultset.getString("name");
+            String crate_at = resultset.getString("create_at");
+
+            return new User(userid, username, null, name, crate_at);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 }
--- src/main/java/com/playdata/todos/servlet/LoginServlet.java
+++ src/main/java/com/playdata/todos/servlet/LoginServlet.java
@@ -1,7 +1,6 @@
 package com.playdata.todos.servlet;
 
 import com.playdata.todos.dao.UserDao;
-import com.playdata.todos.dto.User;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -9,29 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class UserServlet extends HttpServlet {
+public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setStatus(200);
-        req.getRequestDispatcher("views/user.html").forward(req, resp);
-        super.doGet(req, resp);
+        req.getRequestDispatcher("view/login.html").forward(req, resp);
+        //super.doGet(req, resp);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String username = req.getParameter("username");
         String password = req.getParameter("password");
-        String name = req.getParameter("name");
-        System.out.println("username = " + username);
-        System.out.println("password = " + password);
-        System.out.println("name = " + name);
-        //입력값 db로 보내준다
-        User user = new User(null, username, password, name, null);
-        UserDao dao = new UserDao();
-        dao.insert(user);
-        //
-        resp.setStatus(201);
-        resp.sendRedirect("/login");
+        if(new UserDao().login(username, password)) {
+            resp.sendRedirect("/main");
+        } else{
+            resp.sendRedirect("/user");
+        }
         //super.doPost(req, resp);
     }
 }
